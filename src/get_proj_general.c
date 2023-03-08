b72d69899585b54460e514029408e8661b396050
v1j4y <vijay.gopal.c@gmail.com>
2023-03-08 14:50:12 +0100

    Fixing bug.

--- src/get_proj_general.c
+++ src/get_proj_general.c
@@ -69,35 +69,35 @@ void get_proj_general(PetscScalar *valxr,
 
   sq2 = sqrt(2.0);
 
-  //fachole[0] = 1.0/8.0;
-  //fachole[1] = sq2/8.0;
-  //fachole[2] = 1.0/8.0;
-  //fachole[3] = sq2/8.0;
-  //fachole[4] = 2.0/8.0;
-  //fachole[5] = sq2/8.0;
-  //fachole[6] = 1.0/8.0;
-  //fachole[7] = sq2/8.0;
-  //fachole[8] = 1.0/8.0;
+  fachole[0] = 1.0/8.0;
+  fachole[1] = sq2/8.0;
+  fachole[2] = 1.0/8.0;
+  fachole[3] = sq2/8.0;
+  fachole[4] = 2.0/8.0;
+  fachole[5] = sq2/8.0;
+  fachole[6] = 1.0/8.0;
+  fachole[7] = sq2/8.0;
+  fachole[8] = 1.0/8.0;
 
-  //fachole[ 9] = sq2/8.0;
-  //fachole[10] = 2.0/8.0;
-  //fachole[11] = sq2/8.0;
-  //fachole[12] = 2.0/8.0;
-  //fachole[13] = 2.0*sq2/8.0;
-  //fachole[14] = 2.0/8.0;
-  //fachole[15] = sq2/8.0;
-  //fachole[16] = 2.0/8.0;
-  //fachole[17] = sq2/8.0;
+  fachole[ 9] = sq2/8.0;
+  fachole[10] = 2.0/8.0;
+  fachole[11] = sq2/8.0;
+  fachole[12] = 2.0/8.0;
+  fachole[13] = 2.0*sq2/8.0;
+  fachole[14] = 2.0/8.0;
+  fachole[15] = sq2/8.0;
+  fachole[16] = 2.0/8.0;
+  fachole[17] = sq2/8.0;
 
-  //fachole[18] = 1.0/8.0;
-  //fachole[19] = sq2/8.0;
-  //fachole[20] = 1.0/8.0;
-  //fachole[21] = sq2/8.0;
-  //fachole[22] = 2.0/8.0;
-  //fachole[23] = sq2/8.0;
-  //fachole[24] = 1.0/8.0;
-  //fachole[25] = sq2/8.0;
-  //fachole[26] = 1.0/8.0;
+  fachole[18] = 1.0/8.0;
+  fachole[19] = sq2/8.0;
+  fachole[20] = 1.0/8.0;
+  fachole[21] = sq2/8.0;
+  fachole[22] = 2.0/8.0;
+  fachole[23] = sq2/8.0;
+  fachole[24] = 1.0/8.0;
+  fachole[25] = sq2/8.0;
+  fachole[26] = 1.0/8.0;
 
   for(ii=*Istart;ii<*Iend;ii++) {
     iii = ii + 1;
@@ -130,29 +130,29 @@ void get_proj_general(PetscScalar *valxr,
       ms[kk] = msloc;
     }
 
-    // Prepare address in base 6
-    int addbase10,addbase6;
-    addbase6 = 0;
-    addbase10 = 0;
-    for(kk=0;kk<nholes;++kk){
-      addbase10 += ms[kk]*(int)pow(6,kk);
-    }
-    addbase6 = base10ToBase6(addbase10);
-    //for(kk=0;kk<natom;++kk)
-    //  printf(" %d ",ideter[kk]);
-    printf("\n");
-    for(kk=0;kk<nholes;++kk)
-      printf(" %d ",ms[kk]);
-    printf("\n");
-    printf("idx=%d addbase6 = %d nstate=%d \n",charPtrToInt(dictionary_get(dadr,intToCharPtr(addbase6))),addbase6, charPtrToInt(dictionary_get(d, intToCharPtr(addbase6))));
+    //// Prepare address in base 6
+    //int addbase10,addbase6;
+    //addbase6 = 0;
+    //addbase10 = 0;
+    //for(kk=0;kk<nholes;++kk){
+    //  addbase10 += ms[kk]*(int)pow(6,kk);
+    //}
+    //addbase6 = base10ToBase6(addbase10);
+    ////for(kk=0;kk<natom;++kk)
+    ////  printf(" %d ",ideter[kk]);
+    //printf("\n");
+    //for(kk=0;kk<nholes;++kk)
+    //  printf(" %d ",ms[kk]);
+    //printf("\n");
+    //printf("idx=%d addbase6 = %d nstate=%d \n",charPtrToInt(dictionary_get(dadr,intToCharPtr(addbase6))),addbase6, charPtrToInt(dictionary_get(d, intToCharPtr(addbase6))));
 
-    //int idhole = idh[0] * 1 + idh[1] * 3 + idh[2] * 9;
-    int idspin = charPtrToInt(dictionary_get(dadr,intToCharPtr(addbase6)));
-    //double fhole  = fachole[idhole];
-    double fspin  = 1.0/charPtrToInt(dictionary_get(d, intToCharPtr(addbase6)));
-    //projvecmat[idhole + idspin] += valxr[iiii]*fhole*fspin;
-    //projvecmat[idh[2]*3*3*idx + idh[1]*3*idx + idh[0]*idx + idspin] += valxr[iiii]*fhole*fspin;
-    //projvecmat[idhole * (int)(pow(3,nholes)) + idspin] += valxr[iiii]*fhole*fspin;
+    ////int idhole = idh[0] * 1 + idh[1] * 3 + idh[2] * 9;
+    //int idspin = charPtrToInt(dictionary_get(dadr,intToCharPtr(addbase6)));
+    ////double fhole  = fachole[idhole];
+    //double fspin  = 1.0/charPtrToInt(dictionary_get(d, intToCharPtr(addbase6)));
+    ////projvecmat[idhole + idspin] += valxr[iiii]*fhole*fspin;
+    ////projvecmat[idh[2]*3*3*idx + idh[1]*3*idx + idh[0]*idx + idspin] += valxr[iiii]*fhole*fspin;
+    ////projvecmat[idhole * (int)(pow(3,nholes)) + idspin] += valxr[iiii]*fhole*fspin;
 
   }
 
